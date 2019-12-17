c3a29e59f6e5c68bdd59ca743f9fd27a304ef65a
PaulFang <316204580@qq.com>
2019-12-17 10:26:23 +0800

    springcloud init project

---
+++ microservicecloud-config-client-3355/src/main/java/com/webank/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,10 @@
+package com.webank.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by PaulFang on 2019/10/27.
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+}
